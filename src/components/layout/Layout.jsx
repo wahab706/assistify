db98caed50a83bac1c02228a76bdaf3bf7ce60a9
rouf0919 <rouf0919@gmail.com>
2024-08-13 20:22:58 +0500

    changes added

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -5,12 +5,12 @@ import Footer from "../Footer";
 export function Layout() {
   return (
     <div
-      className="flex flex-col font-Cairo py-[60px]
+      className="flex flex-col font-Cairo py-4 lg:py-[60px]
       px-4 lg:px-[34px]"
     >
       <div className="min-h-screen w-full">
         <Header />
-        <main className="flex-grow py-[97px]">
+        <main className="flex-grow py-[110px]">
           <Outlet />
         </main>
       </div>
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,9 +1,13 @@
 import React, { Fragment } from "react";
 import { Button } from "../components/Button";
-import mobile from "../assets/img/mobile.png";
+import bannerIllustration from "../assets/img/banner-illustration.png";
+import bannerImg from "../assets/img/bannerImg.png";
 import navigation from "../assets/img/navigation.svg";
 import mapping from "../assets/img/mapping.svg";
 import tracking from "../assets/img/tracking.svg";
+import safety from "../assets/img/safety.svg";
+import locationInfo from "../assets/img/locationInfo.svg";
+import translation from "../assets/img/translation.svg";
 import cardIllustrationTop from "../assets/img/card-illustration-top.svg";
 import cardIllustrationBottom from "../assets/img/card-illustration-botm.svg";
 import leadsClientsIllust from "../assets/img/leads-clients-illust.svg";
@@ -33,33 +37,38 @@ export const Landing = () => {
   return (
     <Fragment>
       <div className="max-w-[1370px] mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-12 max-w-[1120px] mx-auto gap-14 mb-[195px]">
-          <div className="flex flex-col gap-6 col-span-7">
-            <h3 className="text-[#000] font-semibold text-[64px] leading-[80.64px]">
-              Navigate your way using your camera{" "}
-            </h3>
-            <div className="flex flex-col gap-3">
+        <div className="relative">
+          <div className="grid grid-cols-1 lg:grid-cols-12 max-w-[1120px] mx-auto mb-[195px] gap-20 lg:gap-0">
+            <div className="order-2 lg:order-1 flex flex-col gap-6 lg:col-span-6">
+              <h3 className="text-[#000] font-semibold text-[64px] leading-[70px]">
+                Explore Saudi Arabia with Ease – Your Ultimate Travel Companion
+              </h3>
               <h4 className="text-[#000] font-semibold text-2xl">
-                indoor navigation made easy.
+                Guides, AI Insights, Emergency Services, and Indoor Navigation –
+                All in One App
               </h4>
-              <p className="text-gray-medium font-medium text-base leading-[19.36px]">
-                Make your journey easy & straightforward with Assistfy’s AR
-                capabilities when you are in Shopping Malls, SmartVenus. Use
-                your phone camera and follow route suggestions{" "}
-              </p>
-            </div>
-            <div className="flex gap-6 flex-wrap">
+              <div className="flex gap-[17px] items-center flex-wrap">
+                <Link to="">
+                  <img
+                    src={googleplay}
+                    alt="googleplay"
+                    className="w-[204px]"
+                  />
+                </Link>
+                <Link to="">
+                  <img src={appstore} alt="appstore" className="w-[204px]" />
+                </Link>
+              </div>
               <Button primary className="!font-medium">
                 Contact us
               </Button>
-              <Button outlined>
-                {" "}
-                <span>Learn more</span>
-              </Button>
             </div>
-          </div>
-          <div className="col-span-5 flex justify-center items-center relative mobile-pic pb-28 pt-7">
-            <img src={mobile} alt="mobile" width={254} />
+            <div className="order-1 lg:order-2 lg:col-span-6 flex justify-center items-center">
+              <div className="absolute -z-10 xl:-top-[91px] xl:right-[70px]">
+                <img src={bannerIllustration} alt="bannerIllustration" />
+              </div>
+              <img src={bannerImg} alt="bannerImg" width={499} />
+            </div>
           </div>
         </div>
         <div className="flex flex-col gap-[54px] max-w-[1120px] mx-auto mb-[195px]">
@@ -67,14 +76,9 @@ export const Landing = () => {
             Assistify can help you with
           </h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[50px]">
-            <div className="bg-white border border-[#CDCDCD] p-5 rounded-3xl flex flex-col items-center gap-4 relative card-after">
+            <div className="bg-white border border-[#CDCDCD] p-5 rounded-3xl flex flex-col items-center gap-4">
               <img src={navigation} alt="navigation" className="mb-2" />
-              <img
-                src={cardIllustrationBottom}
-                alt="cardIllustrationBottom"
-                className="absolute top-[60%] left-10 -z-10"
-              />
-              <h3 className="text-black font-semibold text-[32px] leading-[40.32px]">
+              <h3 className="w-full text-start text-black font-semibold text-[32px] leading-[40.32px]">
                 Indoor navigation
               </h3>
               <p className="text-xl leading-[25.4px]">
@@ -85,8 +89,8 @@ export const Landing = () => {
             </div>
             <div className="bg-white border border-[#CDCDCD] p-5 rounded-3xl flex flex-col items-center gap-4">
               <img src={mapping} alt="mapping" className="mb-2" />
-              <h3 className="text-black font-semibold text-[32px] leading-[40.32px]">
-                Digital mapping
+              <h3 className="w-full text-start text-black font-semibold text-[32px] leading-[40.32px]">
+                Plan your trip
               </h3>
               <p className="text-xl leading-[25.4px]">
                 Create interactive 2D and 3D digital indoor floor maps for your
@@ -99,10 +103,10 @@ export const Landing = () => {
               <img
                 src={cardIllustrationTop}
                 alt="cardIllustrationTop"
-                className="absolute bottom-[60%] right-10 -z-10"
+                className="absolute bottom-[81%] right-3 -z-10"
               />
-              <h3 className="text-black font-semibold text-[32px] leading-[40.32px]">
-                Realtime tracking
+              <h3 className="w-full text-start text-black font-semibold text-[32px] leading-[40.32px]">
+                Talk to your AI
               </h3>
               <p className="text-xl leading-[25.4px]">
                 Track assets and staff real time and manage your resources
@@ -110,59 +114,101 @@ export const Landing = () => {
                 incidents
               </p>
             </div>
+            <div className="bg-white border border-[#CDCDCD] p-5 rounded-3xl flex flex-col items-center gap-4 relative card-after">
+              <img src={safety} alt="safety" className="mb-2" />
+              <img
+                src={cardIllustrationBottom}
+                alt="cardIllustrationBottom"
+                className="absolute top-[50%] -left-12 -z-10"
+              />
+              <h3 className="w-full text-start text-black font-semibold text-[32px] leading-[40.32px]">
+                Safety & help
+              </h3>
+              <p className="text-xl leading-[25.4px]">
+                Track assets and staff real time and manage your resources
+                efficiently. You can monitor, set alerts, and response to
+                incidents
+              </p>
+            </div>
+            <div className="bg-white border border-[#CDCDCD] p-5 rounded-3xl flex flex-col items-center gap-4">
+              <img src={locationInfo} alt="locationInfo" className="mb-2" />
+              <h3 className="w-full text-start text-black font-semibold text-[32px] leading-[40.32px]">
+                Location info
+              </h3>
+              <p className="text-xl leading-[25.4px]">
+                Use your camera to scan public landmarks to more insights or
+                just request an instant location and see your location details
+              </p>
+            </div>
+            <div className="bg-white border border-[#CDCDCD] p-5 rounded-3xl flex flex-col items-center gap-4">
+              <img src={translation} alt="translation" />
+              <h3 className="w-full text-start text-black font-semibold text-[32px] leading-[40.32px]">
+                Easy Translation
+              </h3>
+              <p className="text-xl leading-[25.4px]">
+                Dont get lost in translation again, Easy translation for
+                uploading documents, street boards/signs recognition, text
+                prompts or even voice
+              </p>
+            </div>
           </div>
         </div>
-        <div className="flex flex-col gap-[18.5px] max-w-[1120px] mx-auto mb-[313px]">
-          <h2 className="font-semibold text-5xl leading-[60.48px]">
-            How Assistify leads clients
+        <div className="flex flex-col gap-[54px] max-w-[1120px] mx-auto mb-[313px]">
+          <h2 className="font-semibold text-5xl leading-[90px]">
+            Navigate large indoor spaces effortlessly with our advanced indoor
+            navigation system{" "}
           </h2>
-          <div className="grid grid-cols-12 gap-[50px]">
-            <div className="col-span-4 flex flex-col gap-[96px]">
+          <div className="grid grid-cols-12 lg:gap-5 xl:gap-[50px]">
+            <div className="col-span-12 lg:col-span-3 xl:col-span-4 flex flex-col gap-20 lg:gap-10 xl:gap-[95.25px]">
               <div className="flex flex-col gap-4">
-                <h3 className="text-black font-semibold text-[32px] leading-[40.32px]">
+                <h3 className="text-black font-semibold text-[32px] leading-[50.78px]">
                   Real-Time Navigation Insights
                 </h3>
-                <p className="text-xl leading-[25.4px] text-[#5E5E5E]">
+                <p className="text-xl leading-[31.74px] text-[#5E5E5E]">
                   Get detailed navigation information with ETA, distance, and
                   directional guidance at a glance
                 </p>
               </div>
               <div className="flex flex-col gap-4">
-                <h3 className="text-black font-semibold text-[32px] leading-[40.32px]">
+                <h3 className="text-black font-semibold text-[32px] leading-[50.78px]">
                   Enable GPS and Camera
                 </h3>
-                <p className="text-xl leading-[25.4px] text-[#5E5E5E]">
+                <p className="text-xl leading-[31.74px] text-[#5E5E5E]">
                   Ensure GPS and camera access for accurate positioning and
                   enhanced navigation features
                 </p>
               </div>
               <div className="flex flex-col gap-4">
-                <h3 className="text-black font-semibold text-[32px] leading-[40.32px]">
+                <h3 className="text-black font-semibold text-[32px] leading-[50.78px]">
                   Interactive AR Route Guidance
                 </h3>
-                <p className="text-xl leading-[25.4px] text-[#5E5E5E]">
+                <p className="text-xl leading-[31.74px] text-[#5E5E5E]">
                   Experience route guidance with AR: visualize your path through
                   your camera or switch to map view
                 </p>
               </div>
             </div>
-            <div className="col-span-6 relative">
-              <img src={GroupMobile} alt="GroupMobile" />
-              <img
-                src={leadsClientsIllust}
-                alt="leadsClientsIllust"
-                className="absolute bottom-[55%] -z-10"
-              />
-            </div>
-            <div className="col-span-2 flex flex-col gap-[96px]">
-              <div className="text-xl border border-[#87C446] border-dotted p-5 flex justify-center items-center min-w-[183px] min-h-[171px] rounded-[20px] text-center card-border relative">
-                See navigation details
-              </div>
-              <div className="text-xl border border-[#87C446] border-dotted p-5 flex justify-center items-center min-w-[183px] min-h-[171px] rounded-[20px] text-center card-border relative">
-                Keep your phone’s camera/GPS on
-              </div>
-              <div className="text-xl border border-[#87C446] border-dotted p-5 flex justify-center items-center min-w-[183px] min-h-[171px] rounded-[20px] text-center card-border relative">
-                Follow route path
+            <div className="col-span-12 lg:col-span-9 xl:col-span-8 pt-20 lg:mt-0">
+              <div className="grid grid-cols-12 lg:gap-5 xl:gap-[50px]">
+                <div className="col-span-7 md:col-span-9 relative">
+                  <img src={GroupMobile} alt="GroupMobile" />
+                  <img
+                    src={leadsClientsIllust}
+                    alt="leadsClientsIllust"
+                    className="absolute bottom-[56.4%] -z-10 hidden lg:block"
+                  />
+                </div>
+                <div className="col-span-5 md:col-span-3 flex flex-col gap-8 md:gap-[96px] lg:gap-12 xl:gap-[96px] items-end">
+                  <div className="text-xl border border-[#87C446] border-dotted p-5 flex justify-center items-center w-[183px] h-[171px] rounded-[20px] text-center card-border relative">
+                    See navigation details
+                  </div>
+                  <div className="text-xl border border-[#87C446] border-dotted p-5 flex justify-center items-center w-[183px] h-[171px] rounded-[20px] text-center card-border relative">
+                    Keep your phone’s camera/GPS on
+                  </div>
+                  <div className="text-xl border border-[#87C446] border-dotted p-5 flex justify-center items-center w-[183px] h-[171px] rounded-[20px] text-center card-border relative">
+                    Follow route path
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -270,7 +316,7 @@ export const Landing = () => {
             travel experience
           </h2>
           <div className="flex flex-col gap-24">
-            <div className="flex items-center justify-between relative">
+            <div className="flex items-center justify-between relative gap-14 lg:gap-0">
               <div>
                 <img src={publicBuilding} alt="publicBuilding" />
               </div>
@@ -289,12 +335,12 @@ export const Landing = () => {
               <img
                 src={dotGrid}
                 alt="dotGrid"
-                className="absolute right-0 bottom-0"
+                className="absolute right-0 bottom-0 hidden lg:block"
               />
             </div>
-            <div className="flex items-center justify-between relative">
+            <div className="flex items-center justify-between relative gap-14 lg:gap-0">
               <div className="flex-grow max-w-[760px]">
-                <div className="flex flex-col gap-4 items-end">
+                <div className="flex flex-col gap-4 lg:items-end">
                   <h3 className="text-4xl font-semibold leading-[57px] max-w-[546px]">
                     Talk to your companion anytime
                   </h3>
@@ -316,10 +362,10 @@ export const Landing = () => {
               <img
                 src={dotGrid}
                 alt="dotGrid"
-                className="absolute left-0 bottom-10"
+                className="absolute left-0 bottom-10 hidden lg:block"
               />
             </div>
-            <div className="flex items-center justify-between relative">
+            <div className="flex items-center justify-between relative gap-14 lg:gap-0">
               <div>
                 <img src={arLandmark} alt="publicBuilding" />
               </div>
@@ -338,12 +384,12 @@ export const Landing = () => {
               <img
                 src={dotGrid}
                 alt="dotGrid"
-                className="absolute right-0 bottom-0"
+                className="absolute right-0 bottom-0 hidden lg:block"
               />
             </div>
-            <div className="flex items-center justify-between relative">
+            <div className="flex items-center justify-between relative gap-14 lg:gap-0">
               <div className="flex-grow max-w-[760px]">
-                <div className="flex flex-col gap-4 items-end max-w-[634px]">
+                <div className="flex flex-col gap-4 lg:items-end max-w-[634px]">
                   <h3 className="text-4xl font-semibold leading-[57px] max-w-[457px] w-full">
                     Safety is top priority
                   </h3>
@@ -368,7 +414,7 @@ export const Landing = () => {
           <h2 className="font-semibold text-5xl leading-[90px]">
             Leverage tomorrows technologies today
           </h2>
-          <div className="grid grid-cols-1 gap-y-12 lg:gap-y-0 md:grid-cols-2 lg:grid-cols-4">
+          <div className="grid grid-cols-1 gap-y-12 lg:gap-y-0 sm:grid-cols-2 lg:grid-cols-4">
             <div className="flex flex-col gap-3.5 items-center">
               <h5 className="text-xl leading-[37.48px] text-center">
                 Advance Tech AI/AR/BLE
@@ -405,7 +451,7 @@ export const Landing = () => {
         </div>
 
         <div className="max-w-[1120px] mx-auto bg-white py-7.5 px-5 border border-[#EDEDED] rounded-[20px]">
-          <div className="max-w-[747.19px] mx-auto grid grid-cols-12 items-center gap-[70px]">
+          <div className="max-w-[747.19px] mx-auto grid grid-cols-1 md:grid-cols-12 items-center gap-[70px]">
             <div className="col-span-8">
               <div className="flex flex-col gap-4 max-w-[355px]">
                 <h3 className="text-5xl font-semibold leading-[90px]">
@@ -428,7 +474,7 @@ export const Landing = () => {
                 </Link>
               </div>
             </div>
-            <div className="col-span-4">
+            <div className="col-span-4 hidden md:block">
               <img src={phone} alt="phone" />
             </div>
           </div>
